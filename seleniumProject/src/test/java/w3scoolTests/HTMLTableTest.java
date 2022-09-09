7874050bc84d758767f2ded445eafe9e6d80ebf1
NomiAdler <bnaomi1611@gmail.com>
2022-09-09 12:05:49 +0300

    add command line im readeMe

--- seleniumProject/src/test/java/w3scoolTests/HTMLTableTest.java
+++ seleniumProject/src/test/java/w3scoolTests/HTMLTableTest.java
@@ -14,7 +14,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
